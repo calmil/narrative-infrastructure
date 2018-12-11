f2f88863302e6709be15c39600e3632ee7ab4bf9
Caleb Ali <calebalimiller@gmail.com>
2018-12-11 15:25:36 -0500

    v18 is a big mess! Stele still needs a lot of performance improvements

--- version18/main.py
+++ version18/main.py
@@ -44,7 +44,7 @@ class Stele(object):
         pass
 
     def draw(self):
-        stele_labels = pyglet.graphics.Batch()
+        stele_batch = pyglet.graphics.Batch()
         for x in range(0, agent_count + 1):
 
             if x != 0:
@@ -53,22 +53,23 @@ class Stele(object):
                                   x=(x*self.q_width)+(self.q_width/2),
                                   y=window_height-(self.q_height/2),
                                   anchor_x='center',
-                                  batch=stele_labels)
+                                  batch=stele_batch)
                 # Y label
                 pyglet.text.Label(text=str(x),
                                   x=self.q_width/2,
                                   y=window_height-((x*self.q_height)+self.q_height/2),
                                   anchor_x='center',
-                                  batch=stele_labels)
+                                  batch=stele_batch)
 
             for y in range(0, agent_count+1):
-                pyglet.graphics.draw(4,
-                                     pyglet.gl.GL_LINE_LOOP,
-                                     ('v2f',
-                                      [x * self.q_width, y * self.q_height,
-                                       x * self.q_width, y * self.q_height + self.q_height,
-                                       x * self.q_width + self.q_width, y * self.q_height + self.q_height,
-                                       x * self.q_width + self.q_width, y * self.q_height]))
+                stele_batch.add(5,
+                                pyglet.gl.GL_LINE_LOOP, None,
+                                ('v2f',
+                                    [x * self.q_width, y * self.q_height,
+                                     x * self.q_width, y * self.q_height + self.q_height,
+                                     x * self.q_width + self.q_width, y * self.q_height + self.q_height,
+                                     x * self.q_width + self.q_width, y * self.q_height,
+                                     x * self.q_width, y * self.q_height]))
 
                 if x != 0 and y != 0:
                     # index_tag.join(str(x) + " to " + str(y))
@@ -76,18 +77,18 @@ class Stele(object):
                                       x=(x * self.q_width) + self.q_width/2,
                                       y=window_height-((y*self.q_height)+self.q_height/2),
                                       anchor_x='center',
-                                      batch=stele_labels)
+                                      batch=stele_batch)
 
-                if last_interaction == (x, y):
-                    glColor3f(200,100,0)
-                    pyglet.graphics.draw(4, pyglet.gl.GL_QUADS,
-                                        ('v2f',
-                                        [x * self.q_width, y * self.q_height,
-                                         x * self.q_width, y * self.q_height + self.q_height,
-                                         x * self.q_width + self.q_width, y * self.q_height + self.q_height,
-                                         x * self.q_width + self.q_width, y * self.q_height]
-                                        ))
-        stele_labels.draw()
+                # if last_interaction == (x, y):
+                #     glColor3f(200,100,0)
+                #     stele_batch.add(4, pyglet.gl.GL_QUADS, None,
+                #                         ('v2f',
+                #                         [x * self.q_width, y * self.q_height,
+                #                          x * self.q_width, y * self.q_height + self.q_height,
+                #                          x * self.q_width + self.q_width, y * self.q_height + self.q_height,
+                #                          x * self.q_width + self.q_width, y * self.q_height]
+                #                         ))
+        stele_batch.draw()
 
 
 class Agent(pyglet.sprite.Sprite):
